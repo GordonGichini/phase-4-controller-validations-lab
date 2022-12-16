291bd757cb3dec641c6aec6bc08f8124d12f9153
GordonGichini <gordongichini1@gmail.com>
2022-12-17 01:49:18 +0300

    commit tree bee207a1797a0dbabf348da1a3ad3dd11ce9c5b0

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,13 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
+    if author.valid?
+    render json: author, status: :created 
+    else 
+    render json: { errors: author.errors }, status: :unprocessable_entity 
+    end 
+  end 
 
-    render json: author, status: :created
-  end
 
   private
   
